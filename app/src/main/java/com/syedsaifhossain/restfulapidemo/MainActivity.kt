f055630c865d8c7d87f3e0fefe7205c731fe446a
Syed Saif Hossain <syedsaifnibir@gmail.com>
2024-10-11 14:53:54 +0600

    Progressbar has been added to this project and upload

--- app/src/main/java/com/syedsaifhossain/restfulapidemo/MainActivity.kt
+++ app/src/main/java/com/syedsaifhossain/restfulapidemo/MainActivity.kt
@@ -10,9 +10,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
-
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,36 +21,36 @@ class MainActivity : AppCompatActivity() {
 
 
         ProductService.retrofit.getProduct().enqueue(object : Callback<List<UsersItem>> {
-            override fun onResponse(call: Call<List<UsersItem>>, response: Response<List<UsersItem>>) {
-                if (response.isSuccessful){
-
+            override fun onResponse(
+                call: Call<List<UsersItem>>,
+                response: Response<List<UsersItem>>,
+            ) =
+                if (response.isSuccessful) {
                     var users = response.body()
                     var stringBuilder = StringBuilder()
                     users?.forEach(){
 
-                        stringBuilder.append("User Id: ${it.id}"+ "\n")
-                        stringBuilder.append("Name: ${it.name}"+ "\n")
-                        stringBuilder.append("User Name: ${it.username}"+ "\n")
-                        stringBuilder.append("Email: ${it.email}"+ "\n")
-                        stringBuilder.append("Phone: ${it.phone}"+ "\n")
-                        stringBuilder.append("Address: ${it.address}"+ "\n")
-                        stringBuilder.append("Company: ${it.company}"+ "\n")
-                        stringBuilder.append("Website: ${it.website}"+ "\n"+ "\n"+ "\n")
+                        stringBuilder.append("User Id : ${it.id}"+"\n")
+                        stringBuilder.append("Name : ${it.name}"+"\n")
+                        stringBuilder.append("User Name: ${it.username}"+"\n")
+                        stringBuilder.append("Email : ${it.email}"+"\n")
+                        stringBuilder.append("Phone : ${it.phone}"+"\n")
+                        stringBuilder.append("Address : ${it.address}"+"\n")
+                        stringBuilder.append("Company : ${it.company}"+"\n")
+                        stringBuilder.append("Website : ${it.website}"+"\n \n \n")
                     }
 
-                    binding.textId.text = stringBuilder.toString()
-                    binding.name.text = stringBuilder.toString()
-                    binding.userName.text = stringBuilder.toString()
-                    binding.email.text = stringBuilder.toString()
-                    binding.phone.text = stringBuilder.toString()
-                    binding.address.text = stringBuilder.toString()
-                    binding.company.text = stringBuilder.toString()
-                    binding.website.text = stringBuilder.toString()
-                }
-                else {
+                    binding.textId.text = stringBuilder
+                    binding.name.text = stringBuilder
+                    binding.userName.text = stringBuilder
+                    binding.email.text = stringBuilder
+                    binding.phone.text = stringBuilder
+                    binding.address.text = stringBuilder
+                    binding.company.text = stringBuilder
+                    binding.website.text = stringBuilder
+                } else {
                     println("Error: ${response.code()}")
                 }
-            }
 
             override fun onFailure(call: Call<List<UsersItem>>, t: Throwable) {
 
